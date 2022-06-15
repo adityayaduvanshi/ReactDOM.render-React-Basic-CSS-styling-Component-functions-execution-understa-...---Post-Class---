546ffe71b895b9f8ab850268ba2862feaaa2424b
Aditya Yaduvanshi <38696088+adityayaduvanshi@users.noreply.github.com>
2022-06-16 01:08:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,7 @@ export default function App() {
   return (
     <div>
       <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-      <h1 className="h1">My name is Aditya</h1>
-      <button className="button">Click here</button>
+     
     </div>
   );
 }
